5cca8b806fd4304ca13dc675838aefc7d1b64b4d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-16 12:19:53 +0200

    init

--- HowToCreateCustomButtonStyles/ContentView.swift
+++ HowToCreateCustomButtonStyles/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button {} label: {}
+                .buttonStyle(CustomButtonStyle(imageName: "plus.circle", title: "Add a new post"))
+            Spacer()
+                .frame(height: 40)
+            Button {} label: {}
+                .buttonStyle(CustomButtonStyle(imageName: "plus.circle", title: "Other button"))
+        }
+    }
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    let imageName: String
+    let title: String
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Image(systemName: imageName)
+            Text(title)
+        }
+        .padding(12)
+        .background(!configuration.isPressed ? Color.green : Color.gray.opacity(0.3))
+        .foregroundColor(!configuration.isPressed ? .white : .green.opacity(0.9))
+        .cornerRadius(8)
     }
 }
 
