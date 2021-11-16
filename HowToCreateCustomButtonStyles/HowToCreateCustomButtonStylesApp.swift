f7cdfc2aae554f01b91e5d544273c4222a62aa61
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-16 12:01:57 +0200

    Initial Commit

---
+++ HowToCreateCustomButtonStyles/HowToCreateCustomButtonStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateCustomButtonStylesApp.swift
+//  HowToCreateCustomButtonStyles
+//
+//  Created by ramil on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateCustomButtonStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
